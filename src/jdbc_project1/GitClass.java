136c9443cc3030b51375e5d4beb5a128d6d6fe5f
Bhavadeeshnara <98813292+Bhavadeeshnara@users.noreply.github.com>
2024-04-04 14:42:43 +0530

    Update GitClass.java

--- src/jdbc_project1/GitClass.java
+++ src/jdbc_project1/GitClass.java
@@ -2,6 +2,6 @@ package jdbc_project1;
 
 public class GitClass {
 	public static void main(String[] args) {
-		System.out.println("Github attachment.");
+		System.out.println("Github attachment Edited");
 	}
 }
